5a7296461098aa0e239e6d74a2cd431a77229f5f
fade <truefadeoffical@gmail.com>
2023-03-09 00:38:34 +0100

    Improve build script for docker

--- docker/build.sh
+++ docker/build.sh
@@ -1,37 +1,42 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 
 # This script builds the docker image.
 
 ## Check if the cargo command is available
 if ! command -v cargo > /dev/null 2>&1; then
-    echo "(!) Cargo is not installed"
+    echo "[racker-docker]: Build::check: Cargo is not installed!"
     exit 1
 fi
 
 ## Check if the docker command is available
 if ! command -v docker > /dev/null 2>&1; then
-    echo "(!) Docker is not installed"
+    echo "[racker-docker]: Build::check: Docker is not installed!"
     exit 1
 fi
 
 ## Check if the docker daemon is running
 if ! docker info > /dev/null 2>&1; then
-    echo "(!) Docker daemon is not running"
+    echo "[racker-docker]: Build::check: Docker daemon is not running!"
     exit 1
 fi
 
 ## Build and copy the binary
+echo -n "[racker-docker]: Build::cargo_build... "
 cd ".." || exit
-cargo build --release
-cp target/release/racker docker/racker
+cargo build --release > /dev/null 2>&1
+cp target/release/racker docker/racker > /dev/null 2>&1
+echo "OK"
 
 ## Build the docker image
+echo -n "[racker-docker]: Build::docker_build... "
 cd docker || exit
-docker build -t "fadeoffical/racker:latest" .
+docker build -t "fadeoffical/racker:latest" . > /dev/null 2>&1
+echo "OK"
 
 ## Clean up
+echo -n "[racker-docker]: Build::clean... "
 rm racker
+echo "OK"
 
-## Be done with it uwu
-# (p r o f e s s i o n a l i s m)
-echo "(+) Done"
+## Done
+echo "[racker-docker]: Build::done: Build complete!"
