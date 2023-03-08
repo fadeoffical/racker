c723fdd4ecedeb189d05a254b2c03699c7e3d234
fade <truefadeoffical@gmail.com>
2023-03-08 23:51:23 +0100

    Refactor config loading

--- src/config/cli.rs
+++ src/config/cli.rs
@@ -3,6 +3,7 @@ use std::path::PathBuf;
 // use is public so that the parse method can be called outside of this module
 // and we dont have to clap::Parser but instead can use this one (looks cleaner)
 pub(crate) use clap::Parser;
+use crate::config::Config;
 
 pub(crate) const CONFIG_FILE_DEFAULT_PATH: &str = "/etc/racker/racker.json";
 
@@ -17,4 +18,20 @@ pub(crate) struct Cli {
 
     #[clap(long = "Cport")]
     pub(crate) port: Option<u16>,
+}
+
+pub(crate) fn update_config(cli: &Cli, config: &mut Config) {
+    log::trace!("-> config::cli::update_config()");
+
+    if let Some(host) = &cli.host {
+        log::debug!("Setting host to {}", host);
+        config.network.set_host(host);
+    }
+
+    if let Some(port) = &cli.port {
+        log::debug!("Setting port to {}", port);
+        config.network.set_port(*port);
+    }
+
+    log::trace!("<- config::cli::update_config()");
 }
--- src/config/mod.rs
+++ src/config/mod.rs
@@ -3,109 +3,120 @@ use std::error::Error;
 use std::fs;
 use std::fs::File;
 use std::io::Write;
-use std::path::PathBuf;
+use std::path::{Path, PathBuf};
 
 use crate::config::cli::Cli;
-use crate::log_error_and_panic;
 use racker::common::data::Network;
 
 pub(crate) mod cli;
 
 const CONFIG_FILE_DEFAULT_CONTENTS: &str = include_str!("racker.json");
 
-pub(crate) fn load(arguments: Cli) -> Result<Config, Box<dyn Error>> {
-    log::trace!("Entered config::load()");
+pub(crate) fn load(cli: Cli) -> Result<Config, Box<dyn Error>> {
+    log::trace!("-> config::load()");
 
-    let config_file_path = match arguments.config_file {
+    // update config file path if specified via cli arguments
+    let config_file_path = match cli.config_file {
         None => PathBuf::from(cli::CONFIG_FILE_DEFAULT_PATH),
-        Some(path) => path,
+        Some(ref path) => path.to_path_buf(),
     };
-
     log::debug!("Config file path: {:?}", &config_file_path);
 
-    if !&config_file_path.exists() {
-        log::warn!("Config file not found at {:?}", &config_file_path);
-        log::warn!("Creating default config file");
-
-        if let Some(config_file_dir) = config_file_path.parent() {
-            log::debug!("Creating config file directory: {:?}", &config_file_dir);
-            fs::create_dir_all(config_file_dir)?;
-        };
-
-        log::debug!("Creating config file: {:?}", &config_file_path);
-        let mut file = match File::create(&config_file_path) {
-            Ok(file) => file,
-            Err(err) => {
-                log::error!(
-                    "Failed to create config file at {:?}: {}",
-                    &config_file_path,
-                    err
-                );
-
-                log::trace!("Returning config::load()");
-                return Err(Box::new(err));
-            }
-        };
-
-        log::debug!("Writing default config to config file");
-        match file.write_all(CONFIG_FILE_DEFAULT_CONTENTS.as_bytes()) {
-            Ok(_) => log::debug!("Successfully wrote default config to config file"),
-            Err(err) => {
-                log::error!(
-                    "Failed to write to config file at {:?}: {}",
-                    &config_file_path,
-                    err
-                );
-
-                return Err(Box::new(err));
-            }
-        };
-    }
-
-    log::debug!("Opening config file: {:?}", &config_file_path);
-    let file = match File::open(&config_file_path) {
+    let file = match config_file_from_path(&config_file_path) {
         Ok(file) => file,
         Err(err) => {
-            log::error!(
-                "Failed to open config file at {:?}: {}",
-                &config_file_path,
-                err
-            );
-            log::warn!("Using default config");
+            log::error!("Failed to open config file at {:?}: {}", &config_file_path, err);
 
-            log::trace!("Returning config::load()");
-            return Ok(Config::default());
+            log::trace!("<- config::load()");
+            return Err(err);
         }
     };
 
     log::debug!("Parsing config file");
-    let config = match serde_json::from_reader::<&File, Config>(&file) {
+    return match parse_config_file(&file) {
         Ok(mut config) => {
-            if let Some(host) = &arguments.host {
-                log::debug!("Setting host to {}", host);
-                config.network.set_host(host);
-            }
-
-            if let Some(port) = &arguments.port {
-                log::debug!("Setting port to {}", port);
-                config.network.set_port(*port);
-            }
-
             log::debug!("Successfully parsed config file");
+
+            cli::update_config(&cli, &mut config);
+
+            log::trace!("<- config::load()");
             Ok(config)
         }
+        Err(err) => Err(err),
+    };
+}
+
+fn parse_config_file(file: &File) -> Result<Config, Box<dyn Error>> {
+    log::trace!("-> config::parse_config_file()");
+
+    let config = match serde_json::from_reader::<&File, Config>(file) {
+        Ok(config) => config,
         Err(err) => {
             log::error!("Failed to parse config file: {}", err);
-            log::warn!("Using default config");
 
-            log::trace!("Returning config::load()");
-            log_error_and_panic(Box::new(err));
+            log::trace!("<- config::parse_config_file()");
+            return Err(Box::new(err));
         }
     };
 
-    log::trace!("Returning config::load()");
-    config
+    log::trace!("<- config::parse_config_file()");
+    Ok(config)
 }
+
+fn config_file_from_path(path: &Path) -> Result<File, Box<dyn Error>> {
+    log::trace!("-> config::config_file_from_path()");
+
+    if !path.exists() {
+        log::warn!("Config file not found at {:?}", &path);
+        log::warn!("Creating default config file");
+
+        let mut file = match create_config_file(path) {
+            Ok(file) => file,
+            Err(err) => {
+                log::error!("Failed to create config file at {:?}: {}", &path, err);
+                log::trace!("<- config::config_file_from_path()");
+                return Err(err);
+            }
+        };
+
+        log::debug!("Writing default config to config file");
+        if let Err(err) = write_default_config_file(&mut file) {
+            log::error!("Failed to write to config file at {:?}: {}", &path, err);
+            log::trace!("<- config::config_file_from_path()");
+            return Err(err);
+        };
+    }
+
+    let file = File::open(path)?;
+
+    log::trace!("<- config::config_file_from_path()");
+    Ok(file)
+}
+
+fn create_config_file(path: &Path) -> Result<File, Box<dyn Error>> {
+    log::trace!("-> config::create_config_file()");
+
+    if let Some(parent) = path.parent() {
+        if !parent.exists() {
+            log::debug!("Creating config file directory: {:?}", &parent);
+            fs::create_dir_all(parent)?;
+        }
+    }
+    let file = File::create(path)?;
+
+    log::trace!("<- config::create_config_file()");
+    Ok(file)
+}
+
+fn write_default_config_file(file: &mut File) -> Result<(), Box<dyn Error>> {
+    log::trace!("-> config::write_default_config_file()");
+
+    file.write_all(CONFIG_FILE_DEFAULT_CONTENTS.as_bytes())?;
+
+    log::trace!("<- config::write_default_config_file()");
+    Ok(())
+}
+
 
 #[derive(Serialize, Deserialize, Debug, Clone)]
 #[serde(rename_all = "kebab-case")]
@@ -116,7 +127,7 @@ pub(crate) struct Config {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            network: Network::new("127.0.0.1", 527),
+            network: Network::new("0.0.0.0", 527),
         }
     }
 }
